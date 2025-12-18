1ca2687bd78f4a81aec0dc2f51557d814c77c01e
Suvam Kumar Bhola <Suvamkumarbhola2005@gmail.com>
2025-12-19 02:25:58 +0530

    Refactored the SideMenu component in the BasicProject_1

--- BasicProject_1/src/components/SideMenu/MenuItem.jsx
+++ BasicProject_1/src/components/SideMenu/MenuItem.jsx
@@ -1,34 +1,41 @@
-import React, { useState } from 'react'
-import MenuList from './MenuList'
+import React, { useState } from 'react';
+import MenuList from './MenuList';
 
-function MenuItem({item}) {
-    // console.log(item);
-    const [displayCurrent ,setDispalycurrent] = useState({})
+function MenuItem({ item }) {
+  const [displayCurrent, setDisplayCurrent] = useState({});
 
-    function handleToggleChildren(getId){
-        setDispalycurrent({
-            ...displayCurrent,
-            [getId]: !displayCurrent[getId]
-        })
-    }
-    
+  function handleToggleChildren(getId) {
+    setDisplayCurrent({
+      ...displayCurrent,
+      [getId]: !displayCurrent[getId]
+    });
+  }
+
+  const hasChildren = item && item.children && item.children.length;
+
   return (
-    <li>
-        <div>
-              <p>{item.label}</p>
-              {
-                item && item.children && item.children.length ?
-                <span onClick={()=>handleToggleChildren(item.id)}>+</span>
-                : null
-              }
-        </div>
-        {
-            item && item.children && item.children.length > 0 && displayCurrent[item.id] ?
-            <MenuList list={item.children}/>
-            : null 
-        }
+    <li className="list-none">
+      <div
+        className={`flex items-center justify-between py-2 px-3 rounded-md transition-colors duration-200 ${hasChildren ? 'hover:bg-gray-100 cursor-pointer' : ''
+          }`}
+      >
+        <p className="text-gray-800 font-medium text-sm select-none">
+          {item.label}
+        </p>
+        {hasChildren ? (
+          <span
+            onClick={() => handleToggleChildren(item.id)}
+            className="flex items-center justify-center w-6 h-6 text-gray-600 font-bold text-lg transition-transform duration-200"
+          >
+            {displayCurrent[item.id] ? '−' : '+'}
+          </span>
+        ) : null}
+      </div>
+      {item && item.children && item.children.length > 0 && displayCurrent[item.id] ? (
+        <MenuList list={item.children} />
+      ) : null}
     </li>
-  )
+  );
 }
 
-export default MenuItem
+export default MenuItem;
--- BasicProject_1/src/components/SideMenu/MenuList.jsx
+++ BasicProject_1/src/components/SideMenu/MenuList.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import MenuItem from './MenuItem'
-function MenuList({list =[]}) {
+import React, { useState } from 'react';
+import MenuItem from './MenuItem';
+
+function MenuList({ list = [] }) {
   return (
-    <ul className='MenuListContainer'>
-        {
-            list && list.length ?
-            list.map((listItem)=> <MenuItem item={listItem} /> )
-            : null
-        }
+    <ul className="ml-6 mt-2 space-y-1 border-l-2 border-gray-200 pl-4">
+      {list && list.length
+        ? list.map((listItem, index) => (
+          <MenuItem item={listItem} key={`${listItem.id}-${index}`} />
+        ))
+        : null}
     </ul>
-  )
+  );
 }
 
-export default MenuList
+export default MenuList;
